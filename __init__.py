3739c30f5c0c9af488ac7e9a93d02ed0d83abb08
Marek Rudnicki <marekrud@gmail.com>
2013-11-09 00:24:16 +0100

    marek@vanilla

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 from __future__ import division
+from __future__ import print_function
 
 __author__ = "Marek Rudnicki"
+
+import numpy as np
 
 import neuron
 from neuron import h
@@ -57,3 +60,30 @@ def set_fs(fs):
 def show():
     import matplotlib.pyplot as plt
     plt.show()
+
+
+
+def find_action_potentials(voltages, fs):
+
+    aps = []
+    for v in voltages.T:
+        positive = np.array((v > 0), dtype=int)
+        diff = np.diff(positive)
+
+        ## Range of the last AP
+        imin = np.where(diff==+1)[0][-1]
+        imax = np.where(diff==-1)[0][-1]
+
+
+        icenter = np.argmax(v[imin:imax])
+
+        aps.append(imin + icenter)
+
+    aps = np.array(aps) / fs
+
+    return aps
+
+    # import matplotlib.pyplot as plt
+    # plt.imshow(positive, aspect='auto', interpolation='nearest')
+    # plt.show()
+    # print(voltages)
